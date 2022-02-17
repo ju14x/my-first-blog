29fe5d3bf70d12d8977f779b8d5487542897351b
juliax <jfcaiado@hotmail.com>
2022-02-17 13:39:07 -0300

    Migrated Comment model to the database, added it to be displayed and used freely, along with small style changes

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,16 @@
 from dataclasses import fields
 from socket import fromshare # remove from linux hosts since this import is only for windows
 from django import forms
-from .models import Post
+from .models import Post, Comment
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text')
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
+from webbrowser import get
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 
 def post_list(request):
@@ -52,3 +53,16 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list') # the .delete() function works for every django model so there's no need to add it to the Post model
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
